895ebd4242522497123a2a3d57c3ecabc297e704
Maci Camurça <124228949+maci-camurca@users.noreply.github.com>
2025-07-23 23:58:06 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function /*componete*/Home() {
+    return <h1>Héctor, eu amo você. Se você me ama, dá uma risadinha </h1>
+}
+/*Home está com H maiuscula para marcar a converçã do react para componente*/
+/*O sinal para o next.js renderizar a página que queremos*/
+
+/*Para exportar essa função para fora do arquivo*/
+export default Home;
